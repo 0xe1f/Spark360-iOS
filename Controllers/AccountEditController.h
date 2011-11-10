3070474cc56b2dbdbd1271af415c0d64d4ee46d6
Akop Karapetyan <dev@akop.org>
2011-11-10 07:17:33 +0000

    Reshuffling files

---
+++ Controllers/AccountEditController.h
@@ -0,0 +1,41 @@
+//
+//  AccountAddController.h
+//  ListTest
+//
+//  Created by Akop Karapetyan on 8/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UITableViewTextFieldCell.h"
+#import "XboxAccount.h"
+
+#define WRONG_FIELD_COLOR [UIColor colorWithRed:0.7 green:0.0 blue:0.0 alpha:1.0]
+#define GOOD_FIELD_COLOR [UIColor blackColor]
+
+@interface AccountEditController : UIViewController<UITableViewDelegate, UITextFieldDelegate>
+{
+    UIBarButtonItem *saveButton;
+    
+    UITextField *usernameTextField;
+    UITextField *passwordTextField;
+    UIActivityIndicatorView *savingIndicator;
+    
+    XboxAccount *account;
+}
+
+@property (nonatomic, retain) IBOutlet UITableView *tableView;
+
+@property (nonatomic, retain) UITableViewTextFieldCell *usernameCell;
+@property (nonatomic, retain) UITableViewTextFieldCell *passwordCell;
+
+@property (nonatomic, retain) NSString *password, *emailAddress;
+@property (nonatomic, retain) XboxAccount *account;
+
+-(void)validateFields;
+-(void)validationSucceeded;
+-(void)validationFailed:(NSString*)message;
+
+
+@end
---
+++ Controllers/AccountEditController.m
@@ -0,0 +1,368 @@
+//
+//  AccountAddController.m
+//  ListTest
+//
+//  Created by Akop Karapetyan on 8/14/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AccountEditController.h"
+
+#import "BachAppDelegate.h"
+#import "XboxAccount.h"
+
+#import "XboxLiveParser.h"
+
+@implementation AccountEditController
+
+@synthesize usernameCell = _usernameCell;
+@synthesize passwordCell = _passwordCell;
+@synthesize tableView = _tableView;
+@synthesize password, emailAddress;
+@synthesize account;
+
+- (id)initWithNibName:(NSString *)nibNameOrNil 
+               bundle:(NSBundle *)nibBundleOrNil
+{
+    if (self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil]) 
+    {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    saveButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemSave 
+                                                               target:self 
+                                                               action:@selector(save:)];
+    
+    if (account)
+    {
+        self.emailAddress = account.emailAddress;
+        self.password = account.password;
+    }
+    
+    self.navigationItem.title = NSLocalizedString(@"EditAccount", nil);
+    self.navigationItem.rightBarButtonItem = saveButton;	
+}
+
+- (void)didReceiveMemoryWarning
+{
+    // Releases the view if it doesn't have a superview.
+    [super didReceiveMemoryWarning];
+    
+    // Release any cached data, images, etc that aren't in use.
+}
+
+- (void)dealloc
+{
+    self.usernameCell = nil;
+    self.passwordCell = nil;
+    self.tableView = nil;
+    
+    [saveButton release]; 
+    [usernameTextField release];
+    [passwordTextField release]; 
+    [savingIndicator release];
+    [account release];
+    
+    [super dealloc];
+}
+
+#pragma mark Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tv 
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tv numberOfRowsInSection:(NSInteger)section 
+{
+    switch (section) 
+    {
+		case 0:
+            return 2;
+		default:
+            return 0;
+	}
+}
+
+// Customize the appearance of table view cells.
+- (UITableViewCell *)tableView:(UITableView *)tv cellForRowAtIndexPath:(NSIndexPath *)indexPath 
+{
+    if ([indexPath section] == 0) 
+    {
+        if(indexPath.row == 0) 
+        {
+            self.usernameCell = (UITableViewTextFieldCell *)[self.tableView dequeueReusableCellWithIdentifier:@"UsernameCell"];
+            
+            if (self.usernameCell == nil) 
+            {
+                self.usernameCell = [[[UITableViewTextFieldCell alloc] initWithStyle:UITableViewCellStyleDefault 
+                                                                     reuseIdentifier:@"UsernameCell"] autorelease];
+				self.usernameCell.textLabel.text = NSLocalizedString(@"Username", @"");
+                
+				usernameTextField = [self.usernameCell.textField retain];
+				usernameTextField.placeholder = NSLocalizedString(@"XboxLiveUsername", @"");
+				usernameTextField.keyboardType = UIKeyboardTypeDefault;
+				usernameTextField.returnKeyType = UIReturnKeyNext;
+                usernameTextField.autocorrectionType = UITextAutocorrectionTypeNo;
+                usernameTextField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+                usernameTextField.delegate = self;
+                
+                usernameTextField.text = self.emailAddress;
+			}
+            
+            return self.usernameCell;
+        }
+        else if(indexPath.row == 1) 
+        {
+            self.passwordCell = (UITableViewTextFieldCell *)[self.tableView dequeueReusableCellWithIdentifier:@"PasswordCell"];
+            if (self.passwordCell == nil) 
+            {
+                self.passwordCell = [[[UITableViewTextFieldCell alloc] initWithStyle:UITableViewCellStyleDefault 
+                                                                     reuseIdentifier:@"PasswordCell"] autorelease];
+				self.passwordCell.textLabel.text = NSLocalizedString(@"Password", @"");
+                
+				passwordTextField = [self.passwordCell.textField retain];
+				passwordTextField.placeholder = NSLocalizedString(@"XboxLivePassword", @"");
+				passwordTextField.keyboardType = UIKeyboardTypeDefault;
+				passwordTextField.secureTextEntry = YES;
+                passwordTextField.autocorrectionType = UITextAutocorrectionTypeNo;
+                passwordTextField.autocapitalizationType = UITextAutocapitalizationTypeNone;
+                passwordTextField.delegate = self;
+                
+                passwordTextField.text = self.password;
+			}
+            return self.passwordCell;
+        }
+    }
+    
+    // We shouldn't reach this point, but return an empty cell just in case
+    return [[[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"NoCell"] autorelease];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section 
+{
+	switch (section) 
+    {
+        case 0:
+            return NSLocalizedString(@"LoginInfo", nil);
+		default:
+            return nil;
+	}
+}
+
+#pragma mark UITextField methods
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField 
+{
+    if (textField.returnKeyType == UIReturnKeyNext) 
+    {
+        UITableViewCell *cell = (UITableViewCell *)[textField superview];
+        NSIndexPath *indexPath = [self.tableView indexPathForCell:cell];
+        NSIndexPath *nextIndexPath = [NSIndexPath indexPathForRow:(indexPath.row + 1) 
+                                                        inSection:indexPath.section];
+        UITableViewCell *nextCell = [self.tableView cellForRowAtIndexPath:nextIndexPath];
+        
+        if (nextCell) 
+        {
+            for (UIView *subview in [nextCell subviews]) 
+            {
+                if ([subview isKindOfClass:[UITextField class]]) 
+                {
+                    [subview becomeFirstResponder];
+                    break;
+                }
+            }
+        }
+    }
+    
+	[textField resignFirstResponder];
+	return NO;
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range 
+replacementString:(NSString *)string 
+{
+    UITableViewCell *cell = (UITableViewCell *)[textField superview];
+    NSMutableString *result = [NSMutableString stringWithString:textField.text];
+    
+    [result replaceCharactersInRange:range 
+                          withString:string];
+    
+    cell.textLabel.textColor = ([result length] == 0) ? WRONG_FIELD_COLOR : GOOD_FIELD_COLOR;        
+    
+    return YES;
+}
+
+#pragma mark - View lifecycle
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    // Return YES for supported orientations
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+#pragma mark etc
+
+- (void)save:(id)sender 
+{
+    [usernameTextField resignFirstResponder];
+    [passwordTextField resignFirstResponder];
+	
+	if (savingIndicator == nil) 
+    {
+		savingIndicator = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleGray];
+        
+		[savingIndicator setFrame:CGRectMake(0,0,20,20)];
+		[savingIndicator setCenter:CGPointMake(self.tableView.center.x, savingIndicator.center.y)];
+		
+        UIView *aView = [[UIView alloc] init];
+		[aView addSubview:savingIndicator];
+		
+		[self.tableView setTableFooterView:aView];
+        [aView release];
+	}
+    
+	[savingIndicator setHidden:NO];
+	[savingIndicator startAnimating];
+    
+    BOOL validFields = YES;
+    
+    if ([usernameTextField.text isEqualToString:@""]) 
+    {
+        validFields = NO;
+        self.usernameCell.textLabel.textColor = WRONG_FIELD_COLOR;
+    }
+    if ([passwordTextField.text isEqualToString:@""]) 
+    {
+        validFields = NO;
+        self.passwordCell.textLabel.textColor = WRONG_FIELD_COLOR;
+    }
+    
+    if (!validFields)
+    {
+        [self validationFailed:NSLocalizedString(@"ProvideValidUsernameAndPassword", @"")];
+        return;
+    }
+    
+    [self validateFields];
+}
+
+-(void)validateFields 
+{
+    if ([account.emailAddress isEqualToString:usernameTextField.text]
+        && [account.password isEqualToString:passwordTextField.text]) 
+    {
+        // Nothing's changed
+        [self.navigationController popToRootViewControllerAnimated:YES];
+        return;
+    }
+    
+    self.emailAddress = usernameTextField.text;
+    self.password = passwordTextField.text;
+    
+    BachAppDelegate *bachApp = [BachAppDelegate sharedApp];
+    
+    NSManagedObjectContext *context = bachApp.managedObjectContext;
+    NSFetchRequest *request = [[[NSFetchRequest alloc] init] autorelease];
+    
+    [request setEntity:[NSEntityDescription entityForName:@"XboxAccount"
+                                   inManagedObjectContext:context]];
+    [request setPredicate:[NSPredicate predicateWithFormat:@"(emailAddress = %@)", 
+                           self.emailAddress]];
+    
+    NSArray *array = [context executeFetchRequest:request 
+                                            error:nil];
+    
+    if ([array count] > 0 && ![[[array lastObject] objectID] isEqual:[account objectID]])
+    {
+        // Account with that email address already exists
+        
+        [self validationFailed:NSLocalizedString(@"AnAccountAlreadyExists", @"")];
+        return;
+    }
+    
+    saveButton.enabled = NO;
+    [self.navigationItem  setHidesBackButton:YES
+                                    animated:NO];
+    
+    [self performSelectorInBackground:@selector(checkLogin) 
+                           withObject:nil];
+}
+
+-(void)checkLogin
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    
+    XboxLiveParser *parser = [[XboxLiveParser alloc] init];
+    BOOL success = [parser authenticate:self.emailAddress
+                           withPassword:self.password];
+    
+    if (success)
+    {
+        account.emailAddress = self.emailAddress;
+        account.password = self.password;
+        
+        [[account managedObjectContext] save:nil];
+        
+        [parser synchronizeAccount:account];
+    }
+    
+    [parser release];
+    
+    if (success)
+    {
+        [self performSelectorOnMainThread:@selector(validationSucceeded) 
+                               withObject:nil
+                            waitUntilDone:NO];
+    }
+    else
+    {
+        [self performSelectorOnMainThread:@selector(validationFailed:) 
+                               withObject:NSLocalizedString(@"VerifyUsernameAndPassword", @"")
+                            waitUntilDone:NO];
+    }
+    
+    [pool release];
+}
+
+-(void)validationSucceeded
+{
+    [savingIndicator stopAnimating];
+    [savingIndicator setHidden:YES];
+    
+    [self.navigationController popToRootViewControllerAnimated:YES];
+    
+    saveButton.enabled = YES;
+    [self.navigationItem setHidesBackButton:NO 
+                                   animated:NO];
+}
+
+-(void)validationFailed:(NSString*)message
+{
+    [savingIndicator stopAnimating];
+    [savingIndicator setHidden:YES];
+    
+    self.emailAddress = nil;
+    self.password = nil;
+    
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"LoginFailed", @"") 
+                                                        message:message
+                                                       delegate:self
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+    
+    [alertView show];
+    [alertView release];
+    
+    saveButton.enabled = YES;
+    [self.navigationItem setHidesBackButton:NO 
+                                   animated:NO];
+}
+
+@end
